eb5d98ac99fdae166b685c0192f1ead5824ba39d
Shiva Gupta <shivascalerite@gmail.com>
2026-01-27 12:59:14 +0530

    Collapse maxLength and minLength error messages

--- src/error-handlers/maxLength.js
+++ src/error-handlers/maxLength.js
@@ -10,6 +10,8 @@ import * as Instance from "@hyperjump/json-schema/instance/experimental";
 const maxLengthErrorHandler = async (normalizedErrors, instance, localization) => {
   /** @type ErrorObject[] */
   const errors = [];
+  let lowestMaxLength = Infinity;
+  let mostConstrainingLocation = null;
 
   for (const schemaLocation in normalizedErrors["https://json-schema.org/keyword/maxLength"]) {
     if (normalizedErrors["https://json-schema.org/keyword/maxLength"][schemaLocation]) {
@@ -19,10 +21,16 @@ const maxLengthErrorHandler = async (normalizedErrors, instance, localization) =
     const keyword = await getSchema(schemaLocation);
     const maxLength = /** @type number */ (Schema.value(keyword));
 
+    if (maxLength < lowestMaxLength) {
+      lowestMaxLength = maxLength;
+      mostConstrainingLocation = schemaLocation;
+    }
+  }
+  if (mostConstrainingLocation !== null) {
     errors.push({
-      message: localization.getMaxLengthErrorMessage(maxLength),
+      message: localization.getMaxLengthErrorMessage(lowestMaxLength),
       instanceLocation: Instance.uri(instance),
-      schemaLocations: [schemaLocation]
+      schemaLocations: [mostConstrainingLocation]
     });
   }
 
