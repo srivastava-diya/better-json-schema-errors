626bdc0ea939ef4888a3430e95eb1a5a7a2a9a77
Samail Ahmad <146630631+Sam-61s@users.noreply.github.com>
2026-01-30 04:38:49 +0530

    feat: combine `multipleOf` errors into a single message (#150)
    * combined multipleOf messages
    
    * all redefinement are resolved
    
    * added 3-value test case

--- src/error-handlers/multipleOf.js
+++ src/error-handlers/multipleOf.js
@@ -11,6 +11,11 @@ const multipleOfErrorHandler = async (normalizedErrors, instance, localization) 
   /** @type ErrorObject[] */
   const errors = [];
 
+  /** @type (number | null) */
+  let combinedMultipleOf = null;
+  /** @type string[] */
+  const schemaLocations = [];
+
   for (const schemaLocation in normalizedErrors["https://json-schema.org/keyword/multipleOf"]) {
     if (normalizedErrors["https://json-schema.org/keyword/multipleOf"][schemaLocation]) {
       continue;
@@ -19,14 +24,42 @@ const multipleOfErrorHandler = async (normalizedErrors, instance, localization) 
     const keyword = await getSchema(schemaLocation);
     const multipleOf = /** @type number */ (Schema.value(keyword));
 
+    combinedMultipleOf = combinedMultipleOf === null ? multipleOf : lcm(combinedMultipleOf, multipleOf);
+    schemaLocations.push(schemaLocation);
+  }
+
+  if (combinedMultipleOf !== null) {
     errors.push({
-      message: localization.getMultipleOfErrorMessage(multipleOf),
+      message: localization.getMultipleOfErrorMessage(combinedMultipleOf),
       instanceLocation: Instance.uri(instance),
-      schemaLocations: [schemaLocation]
+      schemaLocations
     });
   }
 
   return errors;
+};
+
+/**
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+const gcd = (a, b) => {
+  while (b !== 0) {
+    const temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return Math.abs(a);
+};
+
+/**
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+const lcm = (a, b) => {
+  return Math.abs(a * b) / gcd(a, b);
 };
 
 export default multipleOfErrorHandler;
